c006238ee3b010a417edf26177de8450860e6ce3
cliljdn <caliljaudiannn@gmail.com>
2021-03-12 13:47:41 +0800

    added bootstrap

--- src/main.js
+++ src/main.js
@@ -5,8 +5,11 @@ import store from './store'
 
 Vue.config.productionTip = false
 
+//imports for bootstrap
+import './plugins/bootstrap'
+
 new Vue({
-  router,
-  store,
-  render: h => h(App)
+	router,
+	store,
+	render: (h) => h(App),
 }).$mount('#app')
