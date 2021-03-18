ed13b5f3f3fff2a2beaff80de96e53bba3edef69
cent257 <vincentgorospe221@gmail.com>
2021-03-18 21:05:15 +0800

    vincents

--- src/components/calculator.vue
+++ src/components/calculator.vue
@@ -1,38 +1,46 @@
 <template>
   <div>
-    <header>
-      <ul>
-        <li>
-          <div><img src="logo.png" width="70" height="70" /></div>
-        </li>
-        <li>
-          <h1>Lyceum of Subic Bay</h1>
-          <h4>College of Engineering</h4>
-        </li>
-      </ul>
-    </header>
+  <header>
+    <ul>
+      <li class="float">
+        <div><img src="https://i.imgur.com/qO1REGc.png" width="85" height="85" /></div>
+      </li>
+      <li class="float">
+        <h1>Lyceum of Subic Bay</h1>
+        <h4>College of Engineering</h4>
+      </li>
+    </ul>
+  </header>
+  
+  <div class="grid">
+    <div class="row">
+      <div class="col-sm-6">
+    <!-- students-->
+       <div id="sidePanel">
+        <h2>Student</h2>
+        <h2>Student No.</h2>
+        <div>
+          <ul class="dabid" v-for="(list, index) in lists" :key="index">
+            <li id="lname"><br><br>{{ list.name }}</li>
+          </ul>
+        </div>
 
-    <div id="sidePanel">
-      <h2>Students</h2>
-      <div>
-        <ul v-for="(list, index) in lists" :key="index">
-          <li>{{ list.name }}</li>
-        </ul>
       </div>
     </div>
-
+  <!--subjects-->
+  <div class="col-sm-6" id="Ssubjects">
     <div id="box">
-      <h2 style="color: maroon; float: left;">Calculator</h2>
-      <div id="sContainer">
+        <h2 style="color: maroon; float: left;">Calculator</h2>
+        <div id="sContainer">
         <select name="Subject" id="subjects" v-model="selectedSubject">
           <option value="">Select Subject</option>
           <option v-for="(category, index) in selectSubj" :key="index">
             {{ category.name }}
           </option>
         </select>
-      </div>
-
-      <div class="container">
+        </div>
+  
+        <div class="container" id="termcontainer">
         <label for="name">Select Term:</label>
         <select name="Term" id="terms">
           <option value="Prelims">Prelims</option>
@@ -41,86 +49,91 @@
         </select>
       </div>
 
-      <div class="container">
-        <p>
-          Quiz:
-          <input
-            type="text"
-            onkeypress="isNumber"
-            id="quiz1"
-            value="0"
-            onkeyup="setGrades()"
-          />
-          *
-          <input
-            title="Input percentage in decimal"
-            type="text"
-            value=""
-            onkeyup="setGrades()"
-            id="mulQuiz"
-          />
-        </p>
-        <p>
-          Total:
-          <input type="text" id="quizTot" value="50" onkeyup="setGrades()" />
-        </p>
+    <div class="container">
+      <p>
+        Quiz:
+        <input
+          type="text"
+          onkeypress="isNumber"
+          id="quiz1"
+          value="0"
+          onkeyup="setGrades()"
+        />
+        *
+        <input
+          title="Input percentage in decimal"
+          type="text"
+          value=""
+          onkeyup="setGrades()"
+          id="mulQuiz"
+        />
+      </p>
+      <p>
+        Total:
+        <input type="text" id="quizTot" value="50" onkeyup="setGrades()" />
+      </p>
+    </div>
+
+    <div class="container">
+      <p>
+        Exam:
+        <input type="text" id="exam1" value="0" onkeyup="setGrades()" /> *
+        <input
+          title="Input percentage in decimal"
+          type="text"
+          value=""
+          onkeyup="setGrades()"
+          id="mulExam"
+        />
+      </p>
+      <p>
+        Total:
+        <input type="text" id="examTot" value="100" onkeyup="setGrades()" />
+      </p>
+    </div>
+
+    <div class="container">
+      <p>
+        Extra Credit Earned:
+        <input type="text" id="extra" value="0" onkeyup="setGrades();" />
+        *
+        <input
+          title="Input percentage in decimal"
+          type="text"
+          value=""
+          onkeyup="setGrades()"
+          id="mulExtra"
+        />
+      </p>
+      <p>
+        Total:
+        <input type="text" id="extraTot" value="10" onkeyup="setGrades()" />
+      </p>
+    </div>
+
+    <div style="padding: 10px;">
+      <p style="font-size: 25sp;">
+        <b><u style="color: maroon;">Calculated Grade</u></b>
+      </p>
+      <p>TOTAL GRADE: <input type="text" id="total" value="0" /></p>
+      <p>REMARKS: <input type="text" id="remark" value="" /></p>
       </div>
 
-      <div class="container">
-        <p>
-          Exam:
-          <input type="text" id="exam1" value="0" onkeyup="setGrades()" /> *
-          <input
-            title="Input percentage in decimal"
-            type="text"
-            value=""
-            onkeyup="setGrades()"
-            id="mulExam"
-          />
-        </p>
-        <p>
-          Total:
-          <input type="text" id="examTot" value="100" onkeyup="setGrades()" />
-        </p>
-      </div>
-
-      <div class="container">
-        <p>
-          Extra Credit Earned:
-          <input type="text" id="extra" value="0" onkeyup="setGrades();" />
-          *
-          <input
-            title="Input percentage in decimal"
-            type="text"
-            value=""
-            onkeyup="setGrades()"
-            id="mulExtra"
-          />
-        </p>
-        <p>
-          Total:
-          <input type="text" id="extraTot" value="10" onkeyup="setGrades()" />
-        </p>
-      </div>
-
-      <div style="padding: 10px;">
-        <p style="font-size: 25sp;">
-          <b><u style="color: maroon;">Calculated Grade</u></b>
-        </p>
-        <p>TOTAL GRADE: <input type="text" id="total" value="0" /></p>
-        <p>REMARKS: <input type="text" id="remark" value="" /></p>
-      </div>
-
-      <button type="button" class="btn btn-primary">Primary</button>
+      <button type="button" class="btn btn-primary" style="background-color: maroon;">Primary</button>
     </div>
   </div>
+</div>
+</div>
+</div>
 </template>
-
+ 
 <script>
-import app from "./scripts/main";
-export default app;
-</script>
+  import app from "./scripts/main";
+  export default app;
+  </script>
+  
+  <style scoped>
+  @import "./styles/master.css";
+  </style>
+  
 
-<style>
-@import "./styles/master.css";
-</style>
