5364e00af3318af2a92cbac94e2012ced763fc5d
cliljdn <caliljaudiannn@gmail.com>
2021-03-19 20:32:37 +0800

    wip -> pulled master

--- src/components/scripts/main.js
+++ src/components/scripts/main.js
@@ -1,93 +1,90 @@
 export default {
-  computed: {
-    selectSubj() {
-      return this.subjects
-    },
+	computed: {
+		selectSubj() {
+			return this.subjects
+		},
 
-    selectTerm() {
-      return this.Terms
-    },
+		selectTerm() {
+			return this.Terms
+		},
 
-    calculateGrade() {
-      console.log((this.exam / this.mExam) * (50 + 50) * this.tExam)
-      // this.totalPoints = (this.quiz / this.mQuiz) * (50 + 50) * this.tQuiz
-      //   ((this.quiz / this.tQuiz) * 50 + 50) * this.mQuiz +
-      //   ((this.exam / this.tExam) * 50 + 50) * this.mExam +
-      //   ((this.extra / this.tExtra) * 50 + 50) * this.mExtra.toFixed(2)
-      this.totalPoints = (this.quiz / this.tQuiz) * (50 + 50) * this.mQuiz
-      return this.totalPoints ? this.totalPoints : 0
-    },
-  },
+		calculateGrade() {
+			return (this.totalPoints = (
+				((this.quiz / this.tQuiz) * 50 + 50) * this.mQuiz +
+				((this.exam / this.tExam) * 50 + 50) * this.mExam +
+				((this.extra / this.tExtra) * 50 + 50) * this.mExtra
+			).toFixed(2))
+		},
+	},
 
-  data: function() {
-    return {
-      selectedTerm: '',
+	data: function() {
+		return {
+			selectedTerm: '',
 
-      selectedSubject: '',
+			selectedSubject: '',
 
-      selectedStudent: '',
+			selectedStudent: '',
 
-      subjects: [
-        {
-          name: 'Seminars and Fieldtrips',
-          sCode: 'ENGG521',
-          students: [{ name: 'Mario Louis Sigua', studentID: '2015010237' }],
-        },
-      ],
+			subjects: [
+				{
+					name: 'Seminars and Fieldtrips',
+					sCode: 'ENGG521',
+					students: [{ name: 'Mario Louis Sigua', studentID: '2015010237' }],
+				},
+			],
 
-      Terms: [
-        {
-          name: 'Prelims',
-          //criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
-        },
-        {
-          name: 'Midterm',
-          // criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
-        },
-        {
-          name: 'Finals',
-          //criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
-        },
-      ],
+			Terms: [
+				{
+					name: 'Prelims',
+					//criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
+				},
+				{
+					name: 'Midterm',
+					// criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
+				},
+				{
+					name: 'Finals',
+					//criterias: [{ quiz: "", exam: "", extra: "", totalGrade: "" }],
+				},
+			],
 
-      inpSubject: '',
-      inpCode: '',
+			inpSubject: '',
+			inpCode: '',
 
-      lists: [],
+			lists: [],
 
-      quiz: 0,
-      exam: 0,
-      extra: 0,
-      tQuiz: 0,
-      tExam: 0,
-      tExtra: 0,
-      mQuiz: 0,
-      mExam: 0,
-      mExtra: 0,
-      totalPoints: 0,
-    }
-  },
+			quiz: '',
+			exam: '',
+			extra: '',
+			tQuiz: '',
+			tExam: '',
+			tExtra: '',
+			mQuiz: '',
+			mExam: '',
+			mExtra: '',
+			totalPoints: '',
+		}
+	},
 
-  methods: {
-    select: function(category) {
-      this.lists = category.students
-    },
+	methods: {
+		select: function(category) {
+			this.lists = category.students
+		},
 
-    newSubject: function(name, code) {
-      this.subjects.push({ name, code })
-      console.log(this.subjects)
-    },
-  },
+		newSubject: function(name, code) {
+			this.subjects.push({ name, code })
+			console.log(this.subjects)
+		},
+	},
 
-  watch: {
-    selectedSubject: function(subj) {
-      console.log('subject changed' + subj)
-      this.lists = []
-      this.subjects.map((s) => {
-        if (s.name == subj) {
-          this.lists = s.students
-        }
-      })
-    },
-  },
+	watch: {
+		selectedSubject: function(subj) {
+			this.lists = []
+			this.subjects.map((s) => {
+				if (s.name == subj) {
+					this.lists = s.students
+				}
+			})
+		},
+	},
 }
