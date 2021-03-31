41eab2ac943ac6ce9361f8ce7a618f480cd6fbe8
cliljdn <caliljaudiannn@gmail.com>
2021-04-01 02:16:49 +0800

    wip -> pulled master

--- src/components/promptModal.vue
+++ src/components/promptModal.vue
@@ -1,47 +1,50 @@
 <template>
-  <!-- Modal -->
-  <div class="modal" style="display: block">
-    <div class="modal-dialog modal-dialog-centered" role="document">
-      <div class="modal-content">
-        <div class="modal-header">
-          <h5 class="modal-title" id="exampleModalCenterTitle">
-            Success!
-          </h5>
-          <button
-            type="button"
-            class="close"
-            data-dismiss="modal"
-            aria-label="Close"
-          >
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <div class="modal-body">
-          You have successfully registered your account.
-        </div>
-        <div class="modal-footer">
-          <button
-            type="button"
-            class="btn btn-primary"
-            @click="closeModal"
-            data-dismiss="modal"
-          >
-            Close
-          </button>
-        </div>
+      <!-- Modal -->
+      <div class="modal" style="display: block">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                  <div class="modal-content">
+                        <div class="modal-header">
+                              <h5
+                                    class="modal-title"
+                                    id="exampleModalCenterTitle"
+                              >
+                                    Success!
+                              </h5>
+                              <button
+                                    type="button"
+                                    class="close"
+                                    data-dismiss="modal"
+                                    aria-label="Close"
+                              >
+                                    <span aria-hidden="true">&times;</span>
+                              </button>
+                        </div>
+                        <div class="modal-body">
+                              You have successfully registered your account.
+                        </div>
+                        <div class="modal-footer">
+                              <button
+                                    type="button"
+                                    class="btn btn-primary"
+                                    @click="closeModal"
+                                    data-dismiss="modal"
+                              >
+                                    Close
+                              </button>
+                        </div>
+                  </div>
+            </div>
       </div>
-    </div>
-  </div>
 </template>
 
 <script>
-export default {
-  props: ["showModal"],
+      export default {
+            props: ['showModal'],
 
-  methods: {
-    closeModal() {
-      this.$store.state.openModal = false;
-    },
-  },
-};
+            methods: {
+                  closeModal() {
+                        this.$store.state.openModal = false
+                  },
+            },
+      }
 </script>
--- src/components/scripts/login.js
+++ src/components/scripts/login.js
@@ -1,151 +1,163 @@
-import validate from "../../validations/validate";
+import validate from '../../validations/validate'
 
 export default {
-  data() {
-    return {
-      showregister: false,
+      data() {
+            return {
+                  showregister: false,
 
-      isregistered: false,
+                  isregistered: false,
 
-      loginErrors: {
-        email: "",
-        password: "",
+                  loginErrors: {
+                        email: '',
+                        password: '',
+                  },
+
+                  registerErrors: {
+                        email: '',
+                        password: '',
+                        confirm: '',
+                        account_type: '',
+                  },
+
+                  yupOptions: { abortEarly: false, strict: false },
+                  inputCredentials: {
+                        email: '',
+                        password: '',
+                  },
+
+                  registerCredentials: {
+                        email: '',
+                        password: '',
+                        confirm: '',
+                        account_type: '',
+                  },
+            }
       },
 
-      registerErrors: {
-        email: "",
-        password: "",
-        confirm: "",
-        account_type: "",
+      methods: {
+            loginValidate: async function(field) {
+                  let { validateLogin } = validate
+                  try {
+                        await validateLogin.validateAt(
+                              field,
+                              this.inputCredentials,
+                              this.yupOptions
+                        )
+                        this.loginErrors[field] = ''
+                  } catch (err) {
+                        err.inner.forEach((error) => {
+                              this.loginErrors[error.path] = error.message
+                        })
+                  }
+            },
+            registerValidate: async function(field) {
+                  let { registerIsValid } = validate
+                  try {
+                        await registerIsValid.validateAt(
+                              field,
+                              this.registerCredentials,
+                              this.yupOptions
+                        )
+                        this.registerErrors[field] = ''
+                  } catch (err) {
+                        err.inner.forEach((error) => {
+                              this.registerErrors[error.path] = error.message
+                        })
+                  }
+            },
+
+            async sendRegistration() {
+                  let { registerIsValid } = validate
+                  try {
+                        const valid = await registerIsValid.validate(
+                              this.registerCredentials,
+                              this.yupOptions
+                        )
+                        if (valid) {
+                              delete this.registerCredentials.confirm
+                              const res = await this.$axios.post(
+                                    `${this.stateVariables.BASE_URL}/accounts/signup`,
+                                    this.registerCredentials
+                              )
+
+                              if (res.status === 200) {
+                                    this.$store.state.openModal = true
+                                    Object.keys(
+                                          this.registerCredentials
+                                    ).forEach((k) => {
+                                          this.registerCredentials[k] = ''
+                                    })
+
+                                    this.showregister = false
+                              }
+                        }
+                  } catch (err) {
+                        console.log(err)
+                        if (err.response !== undefined) {
+                              this.registerErrors.email =
+                                    err.response.data.message
+                        } else {
+                              err.inner.forEach((error) => {
+                                    this.registerErrors[error.path] =
+                                          error.message
+                              })
+                        }
+                  }
+            },
+
+            async sendLogin() {
+                  let { validateLogin } = validate
+                  try {
+                        const valid = await validateLogin.validate(
+                              this.inputCredentials,
+                              this.yupOptions
+                        )
+                        if (valid) {
+                              const res = await this.$axios.post(
+                                    `${this.stateVariables.BASE_URL}/accounts/login`,
+                                    this.inputCredentials
+                              )
+                              if (res.status === 200) {
+                                    this.$store.commit('set_cookie', res.data)
+                              }
+                        }
+                  } catch (err) {
+                        console.log(err.response)
+                        if (err.response !== undefined) {
+                              if (
+                                    err.response.data.message.includes(
+                                          'password'
+                                    )
+                              ) {
+                                    this.loginErrors.password =
+                                          err.response.data.message
+                              } else {
+                                    this.loginErrors.email =
+                                          err.response.data.message
+                              }
+                        } else {
+                              if (err.inner === undefined) {
+                                    return false
+                              } else {
+                                    err.inner.forEach((error) => {
+                                          if (error.path in this.loginErrors) {
+                                                this.loginErrors[error.path] =
+                                                      error.message
+                                          }
+                                          if (error.path in this.loginErrors) {
+                                                this.loginErrors[error.path] =
+                                                      error.message
+                                          }
+                                    })
+                              }
+                        }
+                  }
+            },
       },
 
-      yupOptions: { abortEarly: false, strict: false },
-      inputCredentials: {
-        email: "",
-        password: "",
+      computed: {
+            stateVariables() {
+                  return this.$store.state
+            },
       },
-
-      registerCredentials: {
-        email: "",
-        password: "",
-        confirm: "",
-        account_type: "",
-      },
-    };
-  },
-
-  methods: {
-    loginValidate: async function(field) {
-      let { validateLogin } = validate;
-      try {
-        await validateLogin.validateAt(
-          field,
-          this.inputCredentials,
-          this.yupOptions
-        );
-        this.loginErrors[field] = "";
-      } catch (err) {
-        err.inner.forEach((error) => {
-          this.loginErrors[error.path] = error.message;
-        });
-      }
-    },
-    registerValidate: async function(field) {
-      let { registerIsValid } = validate;
-      try {
-        await registerIsValid.validateAt(
-          field,
-          this.registerCredentials,
-          this.yupOptions
-        );
-        this.registerErrors[field] = "";
-      } catch (err) {
-        err.inner.forEach((error) => {
-          this.registerErrors[error.path] = error.message;
-        });
-      }
-    },
-
-    async sendRegistration() {
-      let { registerIsValid } = validate;
-      try {
-        const valid = await registerIsValid.validate(
-          this.registerCredentials,
-          this.yupOptions
-        );
-        if (valid) {
-          delete this.registerCredentials.confirm;
-          const res = await this.$axios.post(
-            `${this.stateVariables.BASE_URL}/accounts/signup`,
-            this.registerCredentials
-          );
-
-          if (res.status === 200) {
-            this.$store.state.openModal = true;
-            Object.keys(this.registerCredentials).forEach((k) => {
-              this.registerCredentials[k] = "";
-            });
-
-            this.showregister = false;
-          }
-        }
-      } catch (err) {
-        console.log(err);
-        if (err.response !== undefined) {
-          this.registerErrors.email = err.response.data.message;
-        } else {
-          err.inner.forEach((error) => {
-            this.registerErrors[error.path] = error.message;
-          });
-        }
-      }
-    },
-
-    async sendLogin() {
-      let { validateLogin } = validate;
-      try {
-        const valid = await validateLogin.validate(
-          this.inputCredentials,
-          this.yupOptions
-        );
-        if (valid) {
-          const res = await this.$axios.post(
-            `${this.stateVariables.BASE_URL}/accounts/login`,
-            this.inputCredentials
-          );
-          if (res.status === 200) {
-            this.$store.commit("set_cookie", res.data);
-          }
-        }
-      } catch (err) {
-        console.log(err.response);
-        if (err.response !== undefined) {
-          if (err.response.data.message.includes("password")) {
-            this.loginErrors.password = err.response.data.message;
-          } else {
-            this.loginErrors.email = err.response.data.message;
-          }
-        } else {
-          if (err.inner === undefined) {
-            return false;
-          } else {
-            err.inner.forEach((error) => {
-              if (error.path in this.loginErrors) {
-                this.loginErrors[error.path] = error.message;
-              }
-              if (error.path in this.loginErrors) {
-                this.loginErrors[error.path] = error.message;
-              }
-            });
-          }
-        }
-      }
-    },
-  },
-
-  computed: {
-    stateVariables() {
-      return this.$store.state;
-    },
-  },
-};
+}
