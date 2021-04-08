b763f941724d99a7d9e054e47ede27d497ad3396
cent257 <vincentgorospe221@gmail.com>
2021-04-09 04:48:07 +0800

    add student and drop student

--- src/components/scripts/Studentlist.js
+++ src/components/scripts/Studentlist.js
@@ -1,89 +1,123 @@
-import nav from '../navbar'
+import nav from "../navbar";
 export default {
-      components: {
-            navbar: nav,
-      },
+  components: {
+    navbar: nav,
+  },
 
-      computed: {
-            subjectList() {
-                  return this.$store.state.subjectList
-            },
-      },
+  computed: {
+    subjectList() {
+      return this.$store.state.subjectList;
+    },
+  },
 
-      data() {
-            return {
-                  fields: [
-                        {
-                              key: 'student_id',
-                              sortable: true,
-                        },
-                        {
-                              key: 'lastname',
-                              sortable: true,
-                        },
-                        {
-                              key: 'firstname',
-                              sortable: false,
-                        },
-                        {
-                              key: 'student_course',
-                              label: 'Course',
-                              sortable: true,
-                              // Variant applies to the whole column, including the header and footer
-                              // variant: 'danger'
-                        },
+  data() {
+    return {
+      fields: [
+        {
+          key: "student_id",
+          sortable: true,
+        },
+        {
+          key: "lastname",
+          sortable: true,
+        },
+        {
+          key: "firstname",
+          sortable: false,
+        },
+        {
+          key: "student_course",
+          label: "Course",
+          sortable: true,
+          // Variant applies to the whole column, including the header and footer
+          // variant: 'danger'
+        },
 
-                        'show_details',
-                  ],
+        "show_details",
 
-                  items: [
-                        {
-                              Course: 'BSEE',
-                              first_name: 'Dickerson',
-                              last_name: 'Macdonald',
-                              Student_ID: '2015020055',
-                        },
-                        {
-                              Course: 'BSCOE',
-                              first_name: 'Larsen',
-                              last_name: 'Shaw',
-                              Student_ID: '2015020055',
-                        },
-                        {
-                              Course: 'BSECE',
-                              first_name: 'Geneva',
-                              last_name: 'Wilson',
-                              Student_ID: '2015020055',
-                        },
-                        {
-                              Course: 'BSEEE',
-                              first_name: 'Jami',
-                              last_name: 'Carney',
-                              Student_ID: '2015020055',
-                        },
-                  ],
-            }
-      },
+        "drop_students",
+      ],
 
-      methods: {
-            toggleDetails(row) {
-                  if (row._showDetails) {
-                        this.$set(row, '_showDetails', false)
-                  } else {
-                        this.currentItems.forEach((item) => {
-                              this.$set(item, '_showDetails', false)
-                        })
+      items: [
+        {
+          Course: "BSEE",
+          first_name: "Dickerson",
+          last_name: "Macdonald",
+          Student_ID: "2015020055",
+        },
+        {
+          Course: "BSCOE",
+          first_name: "Larsen",
+          last_name: "Shaw",
+          Student_ID: "2015020055",
+        },
+        {
+          Course: "BSECE",
+          first_name: "Geneva",
+          last_name: "Wilson",
+          Student_ID: "2015020055",
+        },
+        {
+          Course: "BSEEE",
+          first_name: "Jami",
+          last_name: "Carney",
+          Student_ID: "2015020055",
+        },
+      ],
 
-                        this.$nextTick(() => {
-                              this.$set(row, '_showDetails', true)
-                        })
-                  }
-            },
-      },
+      name: "",
+      nameState: null,
+      submittedNames: [],
+    };
+  },
 
-      mounted() {
-            this.$store.dispatch('getEnrolledStudents')
+  methods: {
+    toggleDetails(row) {
+      if (row._showDetails) {
+        this.$set(row, "_showDetails", false);
+      } else {
+        this.currentItems.forEach((item) => {
+          this.$set(item, "_showDetails", false);
+        });
 
-            console.log(this.$store.state.openAccordion)
-      },
-}
+        this.$nextTick(() => {
+          this.$set(row, "_showDetails", true);
+        });
+      }
+    },
+
+    checkFormValidity() {
+      const valid = this.$refs.form.checkValidity();
+      this.nameState = valid;
+      return valid;
+    },
+    resetModal() {
+      this.name = "";
+      this.nameState = null;
+    },
+    handleOk(bvModalEvt) {
+      // Prevent modal from closing
+      bvModalEvt.preventDefault();
+      // Trigger submit handler
+      this.handleSubmit();
+    },
+    handleSubmit() {
+      // Exit when the form isn't valid
+      if (!this.checkFormValidity()) {
+        return;
+      }
+      // Push the name to submitted names
+      this.submittedNames.push(this.name);
+      // Hide the modal manually
+      this.$nextTick(() => {
+        this.$bvModal.hide("modal-prevent-closing");
+      });
+    },
+  },
+
+  mounted() {
+    this.$store.dispatch("getEnrolledStudents");
+
+    console.log(this.$store.state.openAccordion);
+  },
+};
