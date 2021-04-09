3ff4fe6cce2cd98b9ee3dab8a776e4b659018c10
marioSigua <78191833+marioSigua@users.noreply.github.com>
2021-04-09 20:17:17 +0800

    fixed

--- src/components/Studentlist.vue
+++ src/components/Studentlist.vue
@@ -7,117 +7,46 @@
       <b-card
         no-body
         class="mb-1"
-        v-for="(subj, index) in subjectList"
+        v-for="subj in subjectList"
         :key="subj.subject_code"
       >
         <b-card-header header-tag="header" class="p-1" role="tab">
-          <b-button block variant="info" v-b-toggle="'accordion' + index">{{
-            subj.subject_name
-          }}</b-button>
+          <b-button
+            block
+            variant="info"
+            v-b-toggle="'accordion' + subj.subject_code"
+            >{{ subj.subject_name }}</b-button
+          >
         </b-card-header>
 
-                  <b-card
-                        no-body
-                        class="mb-1"
-                        v-for="subj in subjectList"
-                        :key="subj.subject_code"
+        <b-collapse
+          :visible="$store.state.openAccordion === subj.subject_code"
+          :id="'accordion' + subj.subject_code"
+          accordion="my-accordion"
+          role="tabpanel"
+        >
+          <b-card-body>
+            <b-card-text>
+              <div class="studentlist">
+                <h2>Student List</h2>
+                <div class="Studentlist">
+                  <b-table
+                    :items="subj.students"
+                    :fields="fields"
+                    striped
+                    hover
+                    responsive="sm"
                   >
-                        <b-card-header
-                              header-tag="header"
-                              class="p-1"
-                              role="tab"
-                        >
-                              <b-button
-                                    block
-                                    variant="info"
-                                    v-b-toggle="'accordion' + subj.subject_code"
-                                    >{{ subj.subject_name }}</b-button
-                              >
-                        </b-card-header>
-
-                        <b-collapse
-                              :visible="
-                                    $store.state.openAccordion ===
-                                          subj.subject_code
-                              "
-                              :id="'accordion' + subj.subject_code"
-                              accordion="my-accordion"
-                              role="tabpanel"
-                        >
-                              <b-card-body>
-                                    <b-card-text>
-                                          <div class="studentlist">
-                                                <h2>Student List</h2>
-                                                <div class="Studentlist">
-                                                      <b-table
-                                                            :items="
-                                                                  subj.students
-                                                            "
-                                                            :fields="fields"
-                                                            striped
-                                                            hover
-                                                            responsive="sm"
-                                                      >
-                                                            <template
-                                                                  #cell(show_details)="row"
-                                                            >
-                                                                  <b-button
-                                                                        size="sm"
-                                                                        class="mr-2 mr2btn"
-                                                                        @click="
-                                                                              row.toggleDetails
-                                                                        "
-                                                                  >
-                                                                        {{
-                                                                              row.detailsShowing
-                                                                                    ? 'Hide'
-                                                                                    : 'Show'
-                                                                        }}
-                                                                        Details
-                                                                  </b-button>
-                                                            </template>
-
-                                                            <template
-                                                                  #row-details="row"
-                                                            >
-                                                                  <b-card>
-                                                                        <b-row
-                                                                              class="mb-2"
-                                                                        >
-                                                                              <b-col
-                                                                                    sm="3"
-                                                                                    class="text-sm-right"
-                                                                                    ><b
-                                                                                          >Prelim:</b
-                                                                                    ></b-col
-                                                                              >
-                                                                              <b-col
-                                                                                    >{{
-                                                                                          row
-                                                                                                .item
-                                                                                                .prelim_grade
-                                                                                    }}</b-col
-                                                                              >
-                                                                        </b-row>
-
-                                                                        <b-row
-                                                                              class="mb-2"
-                                                                        >
-                                                                              <b-col
-                                                                                    sm="3"
-                                                                                    class="text-sm-right"
-                                                                                    ><b
-                                                                                          >Midterm:</b
-                                                                                    ></b-col
-                                                                              >
-                                                                              <b-col
-                                                                                    >{{
-                                                                                          row
-                                                                                                .item
-                                                                                                .midterm_grade
-                                                                                    }}</b-col
-                                                                              >
-                                                                        </b-row>
+                    <template #cell(show_details)="row">
+                      <b-button
+                        size="sm"
+                        class="mr-2 mr2btn"
+                        @click="row.toggleDetails"
+                      >
+                        {{ row.detailsShowing ? "Hide" : "Show" }}
+                        Details
+                      </b-button>
+                    </template>
 
                     <template #row-details="row">
                       <b-card>
