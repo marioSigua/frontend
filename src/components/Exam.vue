f99c803bcf5002e53727912fd58882407eb5dfec
cent257 <vincentgorospe221@gmail.com>
2021-04-11 19:47:30 +0800

    student list for taking exam or quiz

--- src/components/Exam.vue
+++ src/components/Exam.vue
@@ -28,6 +28,7 @@
                                     >
                                           <b-card-body>
                                                 <!--List of topic-->
+                                        
                                                 <div class="ctn">
                                                       <b-form-select
                                                             v-model="selected"
@@ -64,7 +65,7 @@
                                                       class="btnss"
                                                       >Toggle Collapse A and
                                                       B</b-button
-                                                >
+                                                >   
                                                 <!-- Elements to collapse -->
                                                 <b-collapse
                                                       id="collapse-a"
@@ -205,14 +206,57 @@
                         <option>topic3</option>
                   </b-form-select> -->
 
+                  
+
                   <!--MODAL BUTTON-->
                   <b-button v-b-modal.modal-xl variant="primary" class="modbtn"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-down" viewBox="0 0 16 16">
                         <path fill-rule="evenodd" d="M3.5 10a.5.5 0 0 1-.5-.5v-8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 .5.5v8a.5.5 0 0 1-.5.5h-2a.5.5 0 0 0 0 1h2A1.5 1.5 0 0 0 14 9.5v-8A1.5 1.5 0 0 0 12.5 0h-9A1.5 1.5 0 0 0 2 1.5v8A1.5 1.5 0 0 0 3.5 11h2a.5.5 0 0 0 0-1h-2z"/>
                         <path fill-rule="evenodd" d="M7.646 15.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 14.293V5.5a.5.5 0 0 0-1 0v8.793l-2.146-2.147a.5.5 0 0 0-.708.708l3 3z"/>
                       </svg> Import Question</b-button>
                 
-                  </div>
 
+
+                      <div class="accordion" role="tablist" style="width: 47%; margin-left: 10px;">
+                        <b-card no-body class="mb-1">
+                          <b-card-header header-tag="header" class="p-1" role="tab">
+                            <b-button block v-b-toggle.accordion-1 variant="info"
+                              >Student List</b-button
+                            >
+                          </b-card-header>
+                          <b-collapse
+                            id="accordion-1"
+                            visible
+                            accordion="my-accordion"
+                            role="tabpanel"
+                          >
+                            <b-card-body>
+                              <b-card-text>
+                                  <!--checklist-->
+                                  <div>
+                                      <b-form-group
+                                        label="Select Students who will recieve"
+                                        v-slot="{ ariaDescribedby }"
+                                      >
+                                        <b-form-checkbox-group
+                                          v-model="selected"
+                                          :options="options"
+                                          :aria-describedby="ariaDescribedby"
+                                          name="flavour-2a"
+                                          stacked
+                                        ></b-form-checkbox-group>
+                                      </b-form-group>
+                                    </div>
+                                    <!--checklist-->
+                  
+                                    
+                                    
+                              </b-card-text>
+                            </b-card-body>
+                          </b-collapse>
+                        </b-card>
+                      </div>
+                  </div>
+ 
                   
                   <!-- question body -->
                   <div
@@ -376,3 +420,20 @@ ExamEssay
   }
 }
 </script> -->
+
+<!-- <script>
+      export default {
+        data() {
+          return {
+            selected: [], // Must be an array reference!
+            options: [
+              { text: 'Argarin David', value: 'orange' },
+              { text: 'Sigua Mario', value: 'apple' },
+              { text: 'Gorospe Vincent', value: 'pineapple' },
+              { text: 'Calil San', value: 'grape' }
+            ]
+          }
+        }
+      }
+    </script> -->
+  
--- src/components/ExamForm.vue
+++ src/components/ExamForm.vue
@@ -1,49 +1,19 @@
 <template>
   <div class="Eform">
-    <div class="accordion" role="tablist">
-      <b-card no-body class="mb-1">
-        <b-card-header header-tag="header" class="p-1" role="tab">
-          <b-button block v-b-toggle.accordion-1 variant="info"
-            >Student List</b-button
-          >
-        </b-card-header>
-        <b-collapse
-          id="accordion-1"
-          visible
-          accordion="my-accordion"
-          role="tabpanel"
-        >
-          <b-card-body>
-            <b-card-text>
-                <!--checklist-->
-                <div>
-                    <b-form-group
-                      label="Select Students who will recieve"
-                      v-slot="{ ariaDescribedby }"
-                    >
-                      <b-form-checkbox-group
-                        v-model="selected"
-                        :options="options"
-                        :aria-describedby="ariaDescribedby"
-                        name="flavour-2a"
-                        stacked
-                      ></b-form-checkbox-group>
-                    </b-form-group>
-                  </div>
-                  <!--checklist-->
 
-                  
-                  
-            </b-card-text>
-          </b-card-body>
-        </b-collapse>
-      </b-card>
-    </div>
                     <div class="Term">
                         <div class="clr"></div>
                         <h1>Untitled form</h1>
                         <h5>Desciption</h5>
                     </div>  
+
+                    <div class="Term">
+                      <div class="Info"></div>
+                      <label for="text">First Name:</label>
+                      <input>
+                      <label for="text">Last Name:</label>
+                      <input>
+                  </div>  
 
                     <div class="MCQ">
                        <div class="ui form">
@@ -89,26 +59,14 @@
                         <input type="text" class="idn">
                         
                     </div>  
+                    <div class="btnx">
+                      <b-button variant="primary" class="submit">Submit</b-button>
+                    </div>
+                    
   </div>
 
                
 </template>
-
-<script>
-    export default {
-      data() {
-        return {
-          selected: [], // Must be an array reference!
-          options: [
-            { text: 'Argarin David', value: 'orange' },
-            { text: 'Sigua Mario', value: 'apple' },
-            { text: 'Gorospe Vincent', value: 'pineapple' },
-            { text: 'Calil San', value: 'grape' }
-          ]
-        }
-      }
-    }
-  </script>
 
 <style scoped>
     @import './styles/ExamForm.css';
