bc3de7030caf0749f3f6bf14af7a45d37cb9f452
cent257 <vincentgorospe221@gmail.com>
2021-04-24 20:52:15 +0800

    text for essay ident mcq

--- src/components/StdntIdentification.vue
+++ src/components/StdntIdentification.vue
@@ -15,6 +15,24 @@
                />
           </div>
      </div>
+
+
+     <div>
+          <b-button-close></b-button-close>
+          <div class="Identification">
+              <p>Question goes here</p>
+               <br />
+               <input
+                    v-model="identi.student_answer"
+                    @keydown="preventCopy"
+                    @copy.prevent
+                    @paste.prevent
+                    type="text"
+                    class="idn"
+                    :disabled="!isDisabled"
+               />
+          </div>
+     </div>
 </template>
 
 <script>
--- src/components/StudentMCQ.vue
+++ src/components/StudentMCQ.vue
@@ -46,6 +46,104 @@
                </div>
           </div>
      </div>
+
+
+
+     <div class="MCQ">
+          <b-button-close></b-button-close>
+          <div class="ui form">
+               <div class="grouped fields">
+                    <!-- <textarea
+                         name=""
+                         :value="mcq.question"
+                         id=""
+                         cols="30"
+                         rows="10"
+                    ></textarea> -->
+                 <div class="MCQ">
+          <b-button-close></b-button-close>
+          <div class="ui form">
+               <div class="grouped fields">
+                    <!-- <textarea
+                         name=""
+                         :value="mcq.question"
+                         id=""
+                         cols="30"
+                         rows="10"
+                    ></textarea> -->
+                    <p>Question goes here</p>
+                    <div class="field">
+                         <div class="ui radio checkbox">
+                              <b-form-radio-group
+                                   id="radio-group-1"
+                                   v-model="mcq.student_answer"
+                                   name="radio-options"
+                                   stacked
+                                   :disabled="!isDisabled"
+                              >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.a"
+                                        >{{ choice.a }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.b"
+                                        >{{ choice.b }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.c"
+                                        >{{ choice.c }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.d"
+                                        >{{ choice.d }}</b-form-radio
+                                   >
+                              </b-form-radio-group>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
+
+
+     <div class="field">
+                         <div class="ui radio checkbox">
+                              <b-form-radio-group
+                                   id="radio-group-1"
+                                   v-model="mcq.student_answer"
+                                   name="radio-options"
+                                   stacked
+                                   :disabled="!isDisabled"
+                              >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.a"
+                                        >{{ choice.a }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.b"
+                                        >{{ choice.b }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.c"
+                                        >{{ choice.c }}</b-form-radio
+                                   >
+                                   <b-form-radio
+                                        class="mb-2 unselected"
+                                        :value="choice.d"
+                                        >{{ choice.d }}</b-form-radio
+                                   >
+                              </b-form-radio-group>
+                         </div>
+                    </div>
+               </div>
+          </div>
+     </div>
 </template>
 
 <script>
