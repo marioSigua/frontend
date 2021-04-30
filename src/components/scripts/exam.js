33465cb59ed097b175581b9b2d0457891d87e950
cent257 <vincentgorospe221@gmail.com>
2021-04-30 22:27:25 +0800

    refresh

--- src/components/scripts/exam.js
+++ src/components/scripts/exam.js
@@ -324,6 +324,15 @@ export default {
      },
 
      mounted() {
+          if (localStorage.getItem('reloaded')) {
+               // The page was just reloaded. Clear the value from local storage
+               // so that it will reload the next time this page is visited.
+               localStorage.removeItem('reloaded');
+           } else {
+               // Set a flag so that we know not to reload the page twice.
+               localStorage.setItem('reloaded', '1');
+               location.reload();
+           }
           this.$store.dispatch('profSubjects')
      },
 }
