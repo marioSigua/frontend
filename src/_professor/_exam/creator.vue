2a6fc9bda58e5258c30ff55ca28cd05df97fd826
cliljdn <caliljaudiannn@gmail.com>
2021-05-01 21:23:53 +0800

    pulled revamped

--- src/_professor/_exam/creator.vue
+++ src/_professor/_exam/creator.vue
@@ -1,236 +1,290 @@
 <template lang="html">
-  <div>
-    <div class="selection">
-      <select>
-        <option value="">-- Select Term --</option>
-      </select>
-      <select>
-        <option value="">-- Select Subject --</option>
-      </select>
-      <button type="button" @click="$router.push({ name: 'history' })">
-        History
-      </button>
-      <button @click="importQuestion">Import Question</button>
-    </div>
-    <br />
-
-    <ul>
-      <li v-for="(question, q) in questions" :key="q" class="backdrop">
-        <button id="remove" @click="remove(q)">Remove</button>
-        <div v-if="question.type == 'essay'">
-          <h3>Essay</h3>
-          <textarea></textarea>
-        </div>
-        <div v-else-if="question.type == 'identification'">
-          <h3>Identification</h3>
-
-          Question
-          <div class="wrapper">
-            <tabs :mode="mode">
-              <tab title="Text"><textarea></textarea></tab>
-              <tab title="Image">
-                <input
-                  ref="file"
-                  @change="onFileChange"
-                  type="file"
-                  style="display:none"
-                />
-                <img
-                  class="is-rounded"
-                  height="300"
-                  width="300"
-                  :src="imgUrl ? imgUrl : 'https://i.imgur.com/bCOd9N0.jpg'"
-                  alt="Placeholder image"
-                  @click="$refs.file.click()"
-                />
-                <span class="danger">
-                  {{ error }}
-                </span>
-              </tab>
-            </tabs>
+     <div>
+          <div class="selection">
+               <select>
+                    <option value="">-- Select Term --</option>
+               </select>
+               <select>
+                    <option value="">-- Select Subject --</option>
+                    <option
+                         v-for="(subj, i) in subjectList"
+                         :key="i"
+                         :value="subj.subject_code"
+                         >{{ subj.subject_name }}</option
+                    >
+               </select>
+               <button type="button" @click="$router.push({ name: 'history' })">
+                    History
+               </button>
+               <button @click="importQuestion">Import Question</button>
           </div>
-          Answer
-          <input type="text" name="" value="" />
-        </div>
-        <div v-else>
-          <h3>Multiple Choice</h3>
+          <br />
 
-          Question
-          <div class="wrapper">
-            <tabs :mode="mode">
-              <tab title="Text"><textarea></textarea></tab>
-              <tab title="Image">
-                <input
-                  ref="file"
-                  @change="onFileChange"
-                  type="file"
-                  style="display:none"
-                />
-                <img
-                  class="is-rounded"
-                  height="300"
-                  width="300"
-                  :src="imgUrl ? imgUrl : 'https://i.imgur.com/bCOd9N0.jpg'"
-                  alt="Placeholder image"
-                  @click="$refs.file.click()"
-                />
-                <span class="danger">
-                  {{ error }}
-                </span>
-              </tab>
-            </tabs>
+          <ul>
+               <li v-for="(question, q) in questions" :key="q" class="backdrop">
+                    <button id="remove" @click="remove(q)">Remove</button>
+                    <div v-if="question.type == 'essay'">
+                         <h3>Essay</h3>
+                         <textarea></textarea>
+                    </div>
+                    <div v-else-if="question.type == 'identification'">
+                         <h3>Identification</h3>
+
+                         Question
+                         <div class="wrapper">
+                              <tabs :mode="mode">
+                                   <tab title="Text"><textarea></textarea></tab>
+                                   <tab title="Image">
+                                        <input
+                                             ref="file"
+                                             @change="onFileChange"
+                                             type="file"
+                                             style="display:none"
+                                        />
+                                        <img
+                                             class="is-rounded"
+                                             height="300"
+                                             width="300"
+                                             :src="
+                                                  imgUrl
+                                                       ? imgUrl
+                                                       : 'https://i.imgur.com/bCOd9N0.jpg'
+                                             "
+                                             alt="Placeholder image"
+                                             @click="$refs.file.click()"
+                                        />
+                                        <span class="danger">
+                                             {{ error }}
+                                        </span>
+                                   </tab>
+                              </tabs>
+                         </div>
+                         Answer
+                         <input type="text" name="" value="" />
+                    </div>
+                    <div v-else>
+                         <h3>Multiple Choice</h3>
+
+                         Question
+                         <div class="wrapper">
+                              <tabs :mode="mode">
+                                   <tab title="Text"><textarea></textarea></tab>
+                                   <tab title="Image">
+                                        <input
+                                             ref="file"
+                                             @change="onFileChange"
+                                             type="file"
+                                             style="display:none"
+                                        />
+                                        <img
+                                             class="is-rounded"
+                                             height="300"
+                                             width="300"
+                                             :src="
+                                                  imgUrl
+                                                       ? imgUrl
+                                                       : 'https://i.imgur.com/bCOd9N0.jpg'
+                                             "
+                                             alt="Placeholder image"
+                                             @click="$refs.file.click()"
+                                        />
+                                        <span class="danger">
+                                             {{ error }}
+                                        </span>
+                                   </tab>
+                              </tabs>
+                         </div>
+
+                         Answer
+                         <input type="text" name="" value="" />
+                         Choices:
+                         <input type="text" name="" value="" />
+                         <input type="text" name="" value="" />
+                         <input type="text" name="" value="" />
+                    </div>
+               </li>
+          </ul>
+
+          <div class="selection">
+               <select v-model="newItem">
+                    <option value="">-- New Item --</option>
+                    <option value="essay">Essay</option>
+                    <option value="identification">Identification</option>
+                    <option value="mcq">Multiple Choice</option>
+               </select>
+               <button type="button" @click="add">Add</button>
+
+               <div></div>
+               <button type="button" name="button">Create Form</button>
           </div>
-
-          Answer
-          <input type="text" name="" value="" />
-          Choices:
-          <input type="text" name="" value="" />
-          <input type="text" name="" value="" />
-          <input type="text" name="" value="" />
-        </div>
-      </li>
-    </ul>
-
-    <div class="selection">
-      <select v-model="newItem">
-        <option value="">-- New Item --</option>
-        <option value="essay">Essay</option>
-        <option value="identification">Identification</option>
-        <option value="mcq">Multiple Choice</option>
-      </select>
-      <button type="button" @click="add">Add</button>
-
-      <div></div>
-      <button type="button" name="button">Create Form</button>
-    </div>
-  </div>
+     </div>
 </template>
 
 <script>
-import Tab from "../_tabs/tab";
-import Tabs from "../_tabs/tabs";
+     import Tab from '../_tabs/tab'
+     import Tabs from '../_tabs/tabs'
 
-export default {
-  components: {
-    Tab,
-    Tabs,
-  },
+     export default {
+          components: {
+               Tab,
+               Tabs,
+          },
 
-  data() {
-    return {
-      newItem: "",
-      questions: [],
-    };
-  },
-  methods: {
-    add() {
-      switch (this.newItem) {
-        case "essay":
-          this.questions.push({ type: "essay" });
-          break;
-        case "identification":
-          this.questions.push({ type: "identification" });
-          break;
-        case "mcq":
-          this.questions.push({ type: "mcq" });
-          break;
-        default:
-          alert("Please select a type");
-      }
-      this.newItem = "";
-    },
-    remove(s) {
-      this.questions = this.questions.filter((v, i) => {
-        return !(i == s);
-      });
-    },
-    append(item) {
-      this.questions.push(item);
-    },
-    importQuestion() {
-      this.$emit("import");
-    },
+          data() {
+               return {
+                    newItem: '',
+                    questions: [],
 
-    encodeBase64(file) {
-      return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-        reader.onload = () => resolve(reader.result);
-        reader.onerror = (error) => reject(error);
-      });
-    },
+                    subjectList: [],
 
-    async onFileChange(e) {
-      let imgFormats = ["jpg", "jpeg", "png", "PNG"];
-      const file = e.target.files[0];
-      let fileFormat = file.name.split(".")[1];
+                    imgUrl: '',
 
-      if (!file) {
-        e.preventDefault();
-        return;
-      }
+                    mode: '',
 
-      if (!imgFormats.includes(fileFormat)) {
-        e.preventDefault();
-        this.error = "jpg, jpeg and png are the only file supported";
-        return;
-      }
+                    error: '',
 
-      if (file.size > 1000 * 1000) {
-        e.preventDefault();
-        this.error = "Image must be less than 1mb";
-        return;
-      }
+                    questionBody: {
+                         batch_number: '',
+                         student_answer: '',
+                         form_answer: '',
+                         question_score: '',
 
-      this.error = "";
+                         question_text: '',
 
-      this.imgUrl = URL.createObjectURL(file);
+                         question_image: '',
+                         choices: { a: '', b: '', c: '' },
+                         subject_code: '',
+                    },
+               }
+          },
 
-      this.essayValues.question_image = await this.encodeBase64(file);
-    },
-  },
-};
+          methods: {
+               add() {
+                    let d = Date.now()
+                    switch (this.newItem) {
+                         case 'essay':
+                              this.questions.push({
+                                   type: 'essay',
+                                   form_number: d,
+                                   ...this.questionBody,
+                              })
+
+                              console.log(this.questions)
+                              break
+                         case 'identification':
+                              this.questions.push({ type: 'identification' })
+                              break
+                         case 'mcq':
+                              this.questions.push({ type: 'mcq' })
+                              break
+                         default:
+                              alert('Please select a type')
+                    }
+                    this.newItem = ''
+               },
+               remove(s) {
+                    this.questions = this.questions.filter((v, i) => {
+                         return !(i == s)
+                    })
+               },
+               append(item) {
+                    this.questions.push(item)
+               },
+               importQuestion() {
+                    this.$emit('import')
+               },
+
+               encodeBase64(file) {
+                    return new Promise((resolve, reject) => {
+                         const reader = new FileReader()
+                         reader.readAsDataURL(file)
+                         reader.onload = () => resolve(reader.result)
+                         reader.onerror = (error) => reject(error)
+                    })
+               },
+
+               async onFileChange(e) {
+                    let imgFormats = ['jpg', 'jpeg', 'png', 'PNG']
+                    const file = e.target.files[0]
+                    let fileFormat = file.name.split('.')[1]
+
+                    if (!file) {
+                         e.preventDefault()
+                         return
+                    }
+
+                    if (!imgFormats.includes(fileFormat)) {
+                         e.preventDefault()
+                         this.error =
+                              'jpg, jpeg and png are the only file supported'
+                         return
+                    }
+
+                    if (file.size > 1000 * 1000) {
+                         e.preventDefault()
+                         this.error = 'Image must be less than 1mb'
+                         return
+                    }
+
+                    this.error = ''
+
+                    this.imgUrl = URL.createObjectURL(file)
+
+                    this.essayValues.question_image = await this.encodeBase64(
+                         file
+                    )
+               },
+          },
+
+          mounted() {
+               // call subjects then handle data here
+               this.$store.dispatch('profSubjects').then((subjects) => {
+                    // save locally
+                    this.subjectList = subjects
+               })
+          },
+     }
 </script>
 
 <style lang="css" scoped>
-.selection {
-  display: grid;
-  grid-template-columns: 1fr 1fr 1fr 1fr;
-  grid-gap: 10px;
-}
+     .selection {
+          display: grid;
+          grid-template-columns: 1fr 1fr 1fr 1fr;
+          grid-gap: 10px;
+     }
 
-#remove {
-  float: right;
-}
-#remove:hover {
-  background: rgba(255, 0, 0, 0.4);
-}
+     #remove {
+          float: right;
+     }
+     #remove:hover {
+          background: rgba(255, 0, 0, 0.4);
+     }
 
-h3 {
-  padding-bottom: 10px;
-}
+     h3 {
+          padding-bottom: 10px;
+     }
 
-ul {
-  list-style-type: none;
-}
+     ul {
+          list-style-type: none;
+     }
 
-li {
-  padding: 10px;
-  background: rgba(0, 0, 0, 0.7);
-  border-radius: 10px;
-  margin: 10px auto;
-}
+     li {
+          padding: 10px;
+          background: rgba(0, 0, 0, 0.7);
+          border-radius: 10px;
+          margin: 10px auto;
+     }
 
-textarea,
-input {
-  padding: 10px;
-  border: 1px solid #aaa;
-  border-radius: 10px;
-  background: rgba(0, 0, 0, 0.3);
-  color: white;
-  font-family: inherit;
-  font-size: inherit;
-  width: 100%;
-  margin-bottom: 5px;
-}
+     textarea,
+     input {
+          padding: 10px;
+          border: 1px solid #aaa;
+          border-radius: 10px;
+          background: rgba(0, 0, 0, 0.3);
+          color: white;
+          font-family: inherit;
+          font-size: inherit;
+          width: 100%;
+          margin-bottom: 5px;
+     }
 </style>
--- src/_professor/_tabs/tabs.vue
+++ src/_professor/_tabs/tabs.vue
@@ -1,96 +1,97 @@
 <template lang="html">
-  <div :class="{ tabs__light: mode === 'light' }">
-    <ul class="tabs__header">
-      <li
-        v-for="(tab, index) in tabs"
-        :key="tab.title"
-        @click="selectTab(index)"
-        :class="{ tab__selected: index == selectedIndex }"
-      >
-        {{ tab.title }}
-      </li>
-    </ul>
-    <slot></slot>
-  </div>
+     <div :class="{ tabs__light: mode === 'light' }">
+          <ul class="tabs__header">
+               <li
+                    v-for="(tab, index) in tabs"
+                    :key="tab.title"
+                    @click="selectTab(index)"
+                    :class="{ tab__selected: index == selectedIndex }"
+               >
+                    {{ tab.title }}
+               </li>
+          </ul>
+          <slot></slot>
+     </div>
 </template>
 
 <script>
-export default {
-  props: {
-    mode: {
-      type: String,
-      default: "light",
-    },
-  },
-  data() {
-    return {
-      selectedIndex: 0, // the index of the selected tab,
-      tabs: [], // all of the tabs
-    };
-  },
-  created() {
-    this.tabs = this.$children;
-  },
-  mounted() {
-    this.selectTab(0);
-  },
-  methods: {
-    selectTab(i) {
-      this.selectedIndex = i;
+     export default {
+          props: {
+               mode: {
+                    type: String,
+                    default: 'light',
+               },
+          },
 
-      // loop over all the tabs
-      this.tabs.forEach((tab, index) => {
-        tab.isActive = index === i;
-      });
-    },
-  },
-};
+          data() {
+               return {
+                    selectedIndex: 0, // the index of the selected tab,
+                    tabs: [], // all of the tabs
+               }
+          },
+          created() {
+               this.tabs = this.$children
+          },
+          mounted() {
+               this.selectTab(0)
+          },
+          methods: {
+               selectTab(i) {
+                    this.selectedIndex = i
+
+                    // loop over all the tabs
+                    this.tabs.forEach((tab, index) => {
+                         tab.isActive = index === i
+                    })
+               },
+          },
+     }
 </script>
 
 <style lang="css">
-ul.tabs__header {
-  display: block;
-  list-style: none;
-  margin: 0 0 0 20px;
-  padding: 0;
-}
+     ul.tabs__header {
+          display: block;
+          list-style: none;
+          margin: 0 0 0 20px;
+          padding: 0;
+     }
 
-ul.tabs__header > li {
-  padding: 15px 30px;
-  border-radius: 10px;
-  margin: 0;
-  display: inline-block;
-  margin-right: 5px;
-  cursor: pointer;
-}
+     ul.tabs__header > li {
+          padding: 15px 30px;
+          border-radius: 10px;
+          margin: 0;
+          display: inline-block;
+          margin-right: 5px;
+          cursor: pointer;
+     }
 
-ul.tabs__header > li.tab__selected {
-  font-weight: bold;
-  border-radius: 10px 10px 0 0;
-  border-bottom: 8px solid transparent;
-}
+     ul.tabs__header > li.tab__selected {
+          font-weight: bold;
+          border-radius: 10px 10px 0 0;
+          border-bottom: 8px solid transparent;
+     }
 
-.tab {
-  display: inline-block;
-  color: black;
-  padding: 20px;
-  min-width: 800px;
-  border-radius: 10px;
-  min-height: 150px;
-  max-height: auto;
-}
+     .tab {
+          display: inline-block;
+          color: black;
+          padding: 20px;
+          min-width: 800px;
+          border-radius: 10px;
+          min-height: 150px;
+          max-height: auto;
+     }
 
-.tabs__light .tab {
-  background: rgba(0, 0, 0, 0.3);
-}
+     .tabs__light .tab {
+          background: rgba(0, 0, 0, 0.3);
+     }
 
-.tabs__light li {
-  background: rgba(0, 0, 0, 0.3);
-  color: #aaa;
-}
+     .tabs__light li {
+          background: rgba(0, 0, 0, 0.3);
+          color: #aaa;
+     }
 
-.tabs__light li.tab__selected {
-  background-color: #555;
-  color: white;
-}
+     .tabs__light li.tab__selected {
+          background-color: #555;
+          color: white;
+     }
 </style>
