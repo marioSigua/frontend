7f1429b78ed11089c75bd8cb9ec24006d8af29c4
cliljdn <caliljaudiannn@gmail.com>
2021-05-04 00:54:53 +0800

    wip questionnaire

--- src/_professor/_students/questionnaire.vue
+++ src/_professor/_students/questionnaire.vue
@@ -7,28 +7,62 @@
                     <p v-if="list.question_type === 'text'">
                          {{ list.question }}
                     </p>
+
                     <img v-else :src="list.question" alt="" />
-                    <!-- end of if -->
+
+                    <!-- end of v-if -->
 
                     <div v-if="list.type == 'essay'">
-                         <textarea name="" id="" cols="30" rows="10"></textarea>
+                         <textarea
+                              name=""
+                              id=""
+                              cols="30"
+                              rows="10"
+                              class="text"
+                         ></textarea>
                     </div>
 
                     <div v-else-if="list.type == 'identification'">
-                         <input type="text" />
+                         <div>
+                              <input
+                                   class="ptsInput"
+                                   v-model="list.question_score"
+                              />
+                              <span>pts</span>
+                         </div>
+                         <input type="text" class="input" />
                     </div>
 
                     <div v-else>
                          Choices:
-                         <input :value="list.choices.a" type="radio" name="" />
-                         {{ list.choices.a }}
-                         <input :value="list.choices.b" type="radio" name="" />
-                         {{ list.choices.b }}
-                         <input
-                              :value="list.choices.c"
-                              type="radio"
-                              name=""
-                         />{{ list.choices.c }}
+                         <div>
+                              <input
+                                   type="radio"
+                                   id="male"
+                                   name="gender"
+                                   :value="list.choices.a"
+                              />
+                              <label for="male">{{ list.choices.a }}</label
+                              ><br />
+
+                              <input
+                                   type="radio"
+                                   id="male"
+                                   name="gender"
+                                   :value="list.choices.b"
+                              />
+                              <label for="male">{{ list.choices.b }}</label
+                              ><br />
+
+                              <input
+                                   type="radio"
+                                   id="male"
+                                   name="gender"
+                                   :value="list.choices.c"
+                              />
+                              <label for="male">{{ list.choices.c }}</label
+                              ><br />
+                         </div>
                     </div>
                </li>
           </ul>
@@ -64,39 +98,47 @@
           width: 40%;
           margin-bottom: 10px;
      }
+
      span {
           padding: 10px;
      }
+
      .ptsInput {
           max-width: 50px;
           max-height: 50px;
           padding: 10px;
      }
+
      .selection {
           display: grid;
           grid-template-columns: 1fr 1fr 1fr 1fr;
           grid-gap: 10px;
      }
+
      #remove {
           float: right;
      }
      #remove:hover {
           background: rgba(255, 0, 0, 0.4);
      }
+
      h3 {
           padding-bottom: 10px;
      }
+
      ul {
           list-style-type: none;
      }
+
      li {
           padding: 10px;
           background: rgba(0, 0, 0, 0.7);
           border-radius: 10px;
           margin: 10px auto;
      }
-     textarea,
-     input {
+
+     .text,
+     .input {
           padding: 10px;
           border: 1px solid #aaa;
           border-radius: 10px;
