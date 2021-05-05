382596fc83210bcbcc2860989e4876172e51587b
marioSigua <78191833+marioSigua@users.noreply.github.com>
2021-05-06 05:11:04 +0800

    minor modification

--- src/_professor/_calculator/inputGrade.vue
+++ src/_professor/_calculator/inputGrade.vue
@@ -1,113 +1,110 @@
 <template>
-     <div>
-          <!-- Term -->
-          <label for="name">Select Term:</label>
-          <select v-model="selectedTerm" @change="getGrades">
-               <option value="">Select Term</option>
-               <option v-for="(term, t) in terms" :key="t"> {{ term }} </option>
-          </select>
+  <div>
+    <!-- Term -->
+    <label for="name">Select Term:</label>
+    <select v-model="selectedTerm" @change="getGrades">
+      <option value="">Select Term</option>
+      <option v-for="(term, t) in terms" :key="t"> {{ term }} </option>
+    </select>
 
-          <article>
-               Quiz:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.quiz"
-                    placeholder="Enter Quiz Score"
-               />
-               *
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.mQuiz"
-                    placeholder="0.45"
-               />
+    <article>
+      Quiz:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.quiz"
+        placeholder="Enter Quiz Score"
+      />
+      *
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.mQuiz"
+        placeholder="0.45"
+      />
 
-               Total:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.tQuiz"
-                    placeholder="Enter Total Quiz Score"
-               />
-          </article>
+      Total:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.tQuiz"
+        placeholder="Enter Total Quiz Score"
+      />
+    </article>
 
-          <article>
-               Exam:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.exam"
-                    placeholder="Enter Exam Score"
-               />
-               *
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.mExam"
-                    placeholder="0.5"
-               />
+    <article>
+      Exam:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.exam"
+        placeholder="Enter Exam Score"
+      />
+      *
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.mExam"
+        placeholder="0.5"
+      />
 
-               Total:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.tExam"
-                    placeholder="Enter Total Exam Score"
-               />
-          </article>
+      Total:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.tExam"
+        placeholder="Enter Total Exam Score"
+      />
+    </article>
 
-          <article>
-               Extra:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.extra"
-                    placeholder="Enter Participation Grade"
-               />
-               *
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.mExtra"
-                    placeholder="0.05"
-               />
-               Total:
-               <input
-                    type="number"
-                    @input="handleInput"
-                    v-model.number="criterias.tExtra"
-                    placeholder="Enter Total Participation Grade"
-               />
-          </article>
+    <article>
+      Extra:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.extra"
+        placeholder="Enter Participation Grade"
+      />
+      *
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.mExtra"
+        placeholder="0.05"
+      />
+      Total:
+      <input
+        type="number"
+        @input="handleInput"
+        v-model.number="criterias.tExtra"
+        placeholder="Enter Total Participation Grade"
+      />
+    </article>
 
-          <div>
-               <h4>Calculated Grade</h4>
-               <p v-if="$store.state.calculator.listGrades">
-                    TOTAL GRADE:
-                    <input
-                         :value="$store.state.calculator.listGrades"
-                         disabled
-                    />
-               </p>
+    <div>
+      <h4>Calculated Grade</h4>
+      <p v-if="$store.state.calculator.listGrades">
+        TOTAL GRADE:
+        <input :value="$store.state.calculator.listGrades" disabled />
+      </p>
 
-               <p v-else>
-                    TOTAL GRADE:
+      <p v-else>
+        TOTAL GRADE:
 
-                    <input :value="calculateGrade" disabled />
-               </p>
-               <p>
-                    REMARKS:
-                    <input :value="remarks" disabled />
-               </p>
-          </div>
+        <input :value="calculateGrade" disabled />
+      </p>
+      <p>
+        REMARKS:
+        <input :value="remarks" disabled />
+      </p>
+    </div>
 
-          <button type="button" @click="updateGrade">SAVE</button>
-     </div>
+    <button type="button" @click="updateGrade">SAVE</button>
+  </div>
 </template>
 
 <script>
-     /*
+/*
 
 index.vue
 
@@ -129,173 +126,166 @@ index.vue
 
 */
 
-     export default {
-          props: {
-               // target student
-               // needed for database requests
-               target: {
-                    default() {
-                         return {
-                              student_id: '',
-                              date_created: '',
-                         }
-                    },
-               },
+export default {
+  props: {
+    // target student
+    // needed for database requests
+    target: {
+      default() {
+        return {
+          student_id: "",
+          date_created: "",
+        };
+      },
+    },
 
-               // subject code?
-               // needed for database requests
-               subject: {
-                    default() {
-                         return ''
-                    },
-               },
-          },
+    // subject code?
+    // needed for database requests
+    subject: {
+      default() {
+        return "";
+      },
+    },
+  },
 
-          data() {
-               return {
-                    terms: ['Prelims', 'Midterm', 'Finals'],
-                    selectedTerm: '',
+  data() {
+    return {
+      terms: ["Prelims", "Midterm", "Finals"],
+      selectedTerm: "",
 
-                    // Criterias
-                    // input fields
-                    criterias: {
-                         quiz: '',
-                         exam: '',
-                         extra: '',
-                         tQuiz: '',
-                         tExam: '',
-                         tExtra: '',
-                         mQuiz: '',
-                         mExam: '',
-                         mExtra: '',
-                    },
+      // Criterias
+      // input fields
+      criterias: {
+        quiz: "",
+        exam: "",
+        extra: "",
+        tQuiz: "",
+        tExam: "",
+        tExtra: "",
+        mQuiz: "",
+        mExam: "",
+        mExtra: "",
+      },
 
-                    grade: '',
+      grade: "",
 
-                    // temporary storage of old data during verification
-                    previousPrice: null,
-               }
-          },
-          watch: {
-               // watch changes in target
-               // change in target means change in student
-               // target() {
-               //   // get new set of grades
-               //   this.getGrades();
-               // },
-          },
+      // temporary storage of old data during verification
+      previousPrice: null,
+    };
+  },
+  watch: {
+    // watch changes in target
+    // change in target means change in student
+    // target() {
+    //   // get new set of grades
+    //   this.getGrades();
+    // },
+  },
 
-          methods: {
-               handleInput(e) {
-                    let stringValue = e.target.value.toString()
-                    let regex = /^\d*(\.\d{1,2})?$/
-                    if (!stringValue.match(regex) && this.mQuiz !== '') {
-                         this.criterias.mQuiz = this.previousPrice
-                    }
-                    this.previousPrice = this.criterias.mQuiz
-                    // // new input means new data, and new grade to compute
-                    //  this.calculateGrade;
-               },
+  methods: {
+    handleInput(e) {
+      let stringValue = e.target.value.toString();
+      let regex = /^\d*(\.\d{1,2})?$/;
+      if (!stringValue.match(regex) && this.mQuiz !== "") {
+        this.criterias.mQuiz = this.previousPrice;
+      }
+      this.previousPrice = this.criterias.mQuiz;
+      // // new input means new data, and new grade to compute
+      //  this.calculateGrade;
+    },
 
-               // Updates after selecting a term
-               getGrades() {
-                    // if there is no subject dont fetch to avoid backend bugs
-                    if (this.subject == '') return
+    // Updates after selecting a term
+    getGrades() {
+      // if there is no subject dont fetch to avoid backend bugs
+      const gradeList = {
+        student_id: this.target.student_id,
+        created_at: this.target.date_created,
+        term: this.selectedTerm,
+        subject_code: this.subject,
+      };
 
-                    const gradeList = {
-                         student_id: this.target.student_id,
-                         created_at: this.target.date_created,
-                         term: this.selectedTerm,
-                         subject_code: this.subject,
-                    }
+      if (this.subject == "") return;
+      // handle data here using then()
+      else this.$store.dispatch("getStudentGrade", gradeList);
 
-                    // handle data here using then()
-                    this.$store.dispatch('getStudentGrade', gradeList)
-
-                    /* comment database response for better debugging
+      /* comment database response for better debugging
       {
       prelim_grade:100
       prelim_grade:100
       prelim_grade:100
       }
       */
-               },
+    },
 
-               updateGrade() {
-                    console.log(this.target)
-                    const needs = {
-                         ...this.target,
-                         term: this.selectedTerm,
-                         totalGrade: this.calculateGrade,
-                    }
+    updateGrade() {
+      console.log(this.target);
+      const needs = {
+        ...this.target,
+        term: this.selectedTerm,
+        totalGrade: this.calculateGrade,
+      };
 
-                    // handle data here using then()
-                    // example implementation
-                    // patch protocol in store
-                    this.$store.dispatch('updateGrade', needs)
-               },
-          },
+      // handle data here using then()
+      // example implementation
+      // patch protocol in store
+      this.$store.dispatch("updateGrade", needs);
+    },
+  },
 
-          computed: {
-               remarks() {
-                    if (this.calculateGrade >= 75) {
-                         return 'passed'
-                    } else if (!this.calculateGrade) {
-                         return null
-                    } else {
-                         return 'failed'
-                    }
-               },
+  computed: {
+    remarks() {
+      if (this.calculateGrade >= 75) {
+        return "passed";
+      } else if (!this.calculateGrade) {
+        return null;
+      } else {
+        return "failed";
+      }
+    },
 
-               calculateGrade() {
-                    if (Object.values(this.criterias).some((v) => !v)) {
-                         return 0
-                    } else {
-                         return (
-                              ((this.criterias.quiz / this.criterias.tQuiz) *
-                                   50 +
-                                   50) *
-                                   this.criterias.mQuiz +
-                              ((this.criterias.exam / this.criterias.tExam) *
-                                   50 +
-                                   50) *
-                                   this.criterias.mExam +
-                              ((this.criterias.extra / this.criterias.tExtra) *
-                                   50 +
-                                   50) *
-                                   this.criterias.mExtra.toFixed(2)
-                         )
-                    }
-               },
-          },
-     }
+    calculateGrade() {
+      if (Object.values(this.criterias).some((v) => !v)) {
+        return 0;
+      } else {
+        return (
+          ((this.criterias.quiz / this.criterias.tQuiz) * 50 + 50) *
+            this.criterias.mQuiz +
+          ((this.criterias.exam / this.criterias.tExam) * 50 + 50) *
+            this.criterias.mExam +
+          ((this.criterias.extra / this.criterias.tExtra) * 50 + 50) *
+            this.criterias.mExtra.toFixed(2)
+        );
+      }
+    },
+  },
+};
 </script>
 
 <style scoped>
-     article {
-          padding: 10px;
-          border-bottom: 1px white solid;
-     }
+article {
+  padding: 10px;
+  border-bottom: 1px white solid;
+}
 
-     input {
-          border-radius: 5px;
-          padding-left: 5px;
-          color: black;
-     }
+input {
+  border-radius: 5px;
+  padding-left: 5px;
+  color: black;
+}
 
-     ::placeholder {
-          color: #8f8475;
-          opacity: 1;
-          font-style: italic;
-     }
+::placeholder {
+  color: #8f8475;
+  opacity: 1;
+  font-style: italic;
+}
 
-     :-ms-input-placeholder {
-          color: #8f8475;
-          font-style: italic;
-     }
+:-ms-input-placeholder {
+  color: #8f8475;
+  font-style: italic;
+}
 
-     ::-ms-input-placeholder {
-          color: #8f8475;
-          font-style: italic;
-     }
+::-ms-input-placeholder {
+  color: #8f8475;
+  font-style: italic;
+}
 </style>
